c4e262f87b1bbd398c99f84f1cc9e0afc0aa0ab6
DreamNrr <wzh74371233@163.com>
2017-05-22 18:55:27 +0800

    各个页面的搭建--本地视频

---
+++ app/src/main/java/com/example/wzh/myplayer/activity/SystemVideoPlayerActivity.java
@@ -0,0 +1,15 @@
+package com.example.wzh.myplayer.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.wzh.myplayer.R;
+
+public class SystemVideoPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_system_video_player);
+    }
+}
--- app/src/main/java/com/example/wzh/myplayer/pager/LocalVideoPager.java
+++ app/src/main/java/com/example/wzh/myplayer/pager/LocalVideoPager.java
@@ -1,15 +1,112 @@
 package com.example.wzh.myplayer.pager;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.wzh.myplayer.R;
+import com.example.wzh.myplayer.activity.SystemVideoPlayerActivity;
+import com.example.wzh.myplayer.adapter.LocalVideoAdapter;
+import com.example.wzh.myplayer.domain.MediaItem;
 import com.example.wzh.myplayer.fragment.BaseFragment;
+
+import java.util.ArrayList;
 
 /**
  * Created by WZH on 2017/5/22.
  */
 public class LocalVideoPager extends BaseFragment {
+    private ListView lv;
+    private TextView tv_nodata;
+    private LocalVideoAdapter adapter;
+
+    private ArrayList<MediaItem> mediaItems;
+
+    //重写视图
     @Override
     public View initView() {
-        return null;
+        View view = View.inflate(context, R.layout.fragment_local_video_pager,null);
+        lv = (ListView) view.findViewById(R.id.lv);
+        tv_nodata = (TextView) view.findViewById(R.id.tv_nodata);
+        //设置item的点击事件
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                MediaItem item = adapter.getItem(position);
+                Toast.makeText(context, ""+item.toString(), Toast.LENGTH_SHORT).show();
+
+                //把系统的播放器调起来
+                Intent intent = new Intent(context, SystemVideoPlayerActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("videoList",mediaItems);
+                intent.putExtra("position",position);
+                intent.putExtras(bundle);
+                intent.setDataAndType(Uri.parse(item.getData()),"video/*");
+                startActivity(intent);
+
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        getData();
+    }
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            if(mediaItems != null && mediaItems.size() >0){
+                //有数据
+                tv_nodata.setVisibility(View.GONE);
+                //设置适配器
+                adapter = new LocalVideoAdapter(context,mediaItems);
+                lv.setAdapter(adapter);
+            }else{
+                //没有数据
+                tv_nodata.setVisibility(View.VISIBLE);
+            }
+        }
+    };
+    private void getData() {
+        new Thread(){
+            public void run(){
+                mediaItems = new ArrayList<MediaItem>();
+                ContentResolver resolver = context.getContentResolver();
+                Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                String[] objs = {
+                        MediaStore.Video.Media.DISPLAY_NAME,//视频在sdcard上的名称
+                        MediaStore.Video.Media.DURATION,//视频时长
+                        MediaStore.Video.Media.SIZE,//视频文件的大小
+                        MediaStore.Video.Media.DATA//视频播放地址
+                };
+                Cursor cursor = resolver.query(uri, objs, null, null, null);
+                if(cursor != null) {
+                    while(cursor.moveToNext()) {
+                        String name = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DISPLAY_NAME));
+                        long duration = cursor.getLong(cursor.getColumnIndex(MediaStore.Video.Media.DURATION));
+                        long size = cursor.getLong(cursor.getColumnIndex(MediaStore.Video.Media.SIZE));
+                        String data = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATA));
+                        mediaItems.add(new MediaItem(name,duration,size,data));
+                        handler.sendEmptyMessage(0);
+                    }
+                    cursor.close();
+                }
+            }
+        }.start();
     }
 }
