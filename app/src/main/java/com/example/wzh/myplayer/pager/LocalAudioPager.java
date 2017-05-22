1c898ae32ed3b9e08f93ba38b60ba26915b771ea
DreamNrr <wzh74371233@163.com>
2017-05-22 18:46:57 +0800

    各个fragment设置

--- app/src/main/java/com/example/wzh/myplayer/pager/LocalAudioPager.java
+++ app/src/main/java/com/example/wzh/myplayer/pager/LocalAudioPager.java
@@ -1,6 +1,9 @@
 package com.example.wzh.myplayer.pager;
 
+import android.graphics.Color;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.wzh.myplayer.fragment.BaseFragment;
 
@@ -8,8 +11,21 @@ import com.example.wzh.myplayer.fragment.BaseFragment;
  * Created by WZH on 2017/5/22.
  */
 public class LocalAudioPager extends BaseFragment {
+    private TextView textView;
+
+
     @Override
     public View initView() {
-        return null;
+        textView = new TextView(context);
+        textView.setTextSize(30);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextColor(Color.RED);
+        return textView;
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        textView.setText("本地音乐的内容");
     }
 }
